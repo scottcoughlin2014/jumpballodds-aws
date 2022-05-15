ef2d89b2a1494304142947bacb1601503b1db6e9
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-05-15 16:26:00 -0500

    Going to use AWS cloud formation first and then terraform

---
+++ jumpballodds/prod_settings.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
