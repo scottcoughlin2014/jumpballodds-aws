cba10d78b44f0cb564aaeb3ec4779077488c9ff0
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-05-15 21:07:32 -0500

    We have a working hello world

--- docker/jumpballodds/start-prod-server.sh
+++ docker/jumpballodds/start-prod-server.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 echo "Starting production environment"
-python manage.py migrate
-python manage.py collectstatic --noinput
 gunicorn jumpballodds.wsgi --bind 0.0.0.0:8000 --timeout 60 --access-logfile - --error-logfile -
