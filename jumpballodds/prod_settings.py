71e257f96bd416b796968d8c12c392c916f3fb24
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-05-15 20:04:53 -0500

    Let's try cloud formation using docker-compose to deploy our django app on our EC2 instance

--- jumpballodds/prod_settings.py
+++ jumpballodds/prod_settings.py
@@ -3,12 +3,12 @@ from .settings import *
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
